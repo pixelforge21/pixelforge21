5876b0d890953e45560bc400b6158cac8d26d4be
pixelforge21 <serverdestroyerr@gmail.com>
2025-07-20 10:24:29 +0530

    Update tailwindcss.config.js

--- tailwindcss.config.js
+++ tailwindcss.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src//*.{js,jsx,ts,tsx}",
+    "./src//*.{js,jsx,ts,tsx}", // Scan all JSX/JS files inside src
     "./public/index.html"
   ],
   theme: {
