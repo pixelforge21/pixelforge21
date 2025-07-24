d46c276383d03c7c8ee1e2bc4b0be9faee28537a
pixelforge21 <serverdestroyerr@gmail.com>
2025-07-24 12:46:06 +0530

    Update generateOTP.js

--- frontend/src/utils/helpers/generateOTP.js
+++ frontend/src/utils/helpers/generateOTP.js
@@ -1,1 +1,9 @@
 
+export default function generateOTP(length = 6) {
+  let otp = "";
+  for (let i = 0; i < length; i++) {
+    otp += Math.floor(Math.random() * 10).toString(); // Only digits
+  }
+  return otp;
+}
+
