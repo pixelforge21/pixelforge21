a381e80ce76ef22ba04bcd11acf240eb24c4e005
pixelforge21 <serverdestroyerr@gmail.com>
2025-07-24 21:43:57 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,11 @@ import Home from './pages/Home';
 import ProductPage from './pages/ProductPage';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import OTPVerification from './pages/OTPVerification';
+import OTPVerification from './pages/OTPVerify';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
 import Profile from './pages/Profile';
-import Orders from './pages/Orders';
-import Offers from './pages/Offers';
+import Orders from './pages/MyOrder';
 import Support from './pages/Support';
 import NotFound from './pages/NotFound';
 
