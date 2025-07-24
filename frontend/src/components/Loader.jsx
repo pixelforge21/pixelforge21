e539db009387b87dd5abd2d881ba5e7f470c76d7
pixelforge21 <serverdestroyerr@gmail.com>
2025-07-24 13:12:09 +0530

    Update Loader.jsx

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,11 +1,7 @@
-import React from "react";
-
-const Loader = () => {
+export default function Loader() {
   return (
-    <div className="flex justify-center items-center h-64">
-      <div className="w-12 h-12 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+    <div className="flex items-center justify-center h-40">
+      <div className="w-10 h-10 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
     </div>
   );
-};
-
-export default Loader;
+}
