ce5b4d8f3e0fdb99db2fbaf1c28798e006a8a285
pixelforge21 <serverdestroyerr@gmail.com>
2025-07-24 12:44:56 +0530

    Update axiosInstance.js

--- frontend/src/utils/api/axiosInstance.js
+++ frontend/src/utils/api/axiosInstance.js
@@ -1,1 +1,11 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "https://pixelforge21.onrender.com", // Backend root URL
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
 
