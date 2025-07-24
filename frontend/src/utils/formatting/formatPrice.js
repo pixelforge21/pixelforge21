71ab782b8015082d56c6b149daca84c8b41adc29
pixelforge21 <serverdestroyerr@gmail.com>
2025-07-24 12:43:45 +0530

    Update formatPrice.js

--- frontend/src/utils/formatting/formatPrice.js
+++ frontend/src/utils/formatting/formatPrice.js
@@ -1,1 +1,9 @@
+export default function formatPrice(amount) {
+  if (isNaN(amount)) return "₹0.00";
+  return new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+    minimumFractionDigits: 2,
+  }).format(amount);
+}
 
