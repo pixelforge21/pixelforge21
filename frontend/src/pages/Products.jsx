39e6675d0749082baecf76062fa31d61349f00e5
pixelforge21 <serverdestroyerr@gmail.com>
2025-07-25 22:13:29 +0530

    Create Products.jsx

---
+++ frontend/src/pages/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Products = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">All Products</h1>
+      <p>List of all products will appear here.</p>
+    </div>
+  );
+};
+
+export default Products;
