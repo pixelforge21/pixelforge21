75f86b951a726f2e9cc583ebcf6771d64e9c98af
pixelforge21 <serverdestroyerr@gmail.com>
2025-07-25 23:00:00 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,8 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'; // <- Tailwind styles
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
